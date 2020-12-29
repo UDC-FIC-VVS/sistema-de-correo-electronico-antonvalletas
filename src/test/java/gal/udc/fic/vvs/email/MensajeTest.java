e7c5147450f3767adf275fda0c38e0eea1029a6a
Antón Valladares <anton.valladares@udc.es>
2020-12-29 21:19:46 +0100

    Completed tests for all Mensaje.java methods

--- src/test/java/gal/udc/fic/vvs/email/MensajeTest.java
+++ src/test/java/gal/udc/fic/vvs/email/MensajeTest.java
@@ -1,6 +1,7 @@
 package gal.udc.fic.vvs.email;
 
 import gal.udc.fic.vvs.email.archivo.Texto;
+import gal.udc.fic.vvs.email.correo.Correo;
 import gal.udc.fic.vvs.email.correo.Mensaje;
 import org.junit.Test;
 
@@ -38,5 +39,60 @@ public class MensajeTest {
 		mensaje.establecerLeido(true);
 		assertEquals(0, mensaje.obtenerNoLeidos());
 
+	}
+
+	/*Tests obtenerTamanho method of Mensaje*/
+	@Test
+	public void mensajeObtenerTamanhoTest() {
+		String messageContents = "prueba de contenido de mensaje";
+		String messageContentsName = "nombre del contenido";
+		Mensaje mensaje = new Mensaje(new Texto(messageContentsName, messageContents));
+		assertEquals(messageContents.length(), mensaje.obtenerTamaño());
+	}
+
+	/*Test icono 1*/
+	@Test
+	public void mensajeObtenerIconoNoLeidoTest() {
+		String messageContents = "prueba de contenido de mensaje";
+		String messageContentsName = "nombre del contenido";
+		Mensaje mensaje = new Mensaje(new Texto(messageContentsName, messageContents));
+		assertEquals(Correo.ICONO_NUEVO_MENSAJE, mensaje.obtenerIcono());
+	}
+
+	/*Test icono 2*/
+	@Test
+	public void mensajeObtenerIconoLeidoTest() {
+		String messageContents = "prueba de contenido de mensaje";
+		String messageContentsName = "nombre del contenido";
+		Mensaje mensaje = new Mensaje(new Texto(messageContentsName, messageContents));
+		mensaje.establecerLeido(true);
+		assertEquals(Correo.ICONO_MENSAJE, mensaje.obtenerIcono());
+	}
+
+	/*Test previsualizacion*/
+	@Test
+	public void mensajeObtenerPreVisualizacionTest() {
+		String messageContents = "prueba de contenido de mensaje";
+		String messageContentsName = "nombre del contenido";
+		Mensaje mensaje = new Mensaje(new Texto(messageContentsName, messageContents));
+		assertEquals(messageContents.substring(0, Math.min(messageContents.length(), 32)) + "...", mensaje.obtenerPreVisualizacion());
+	}
+
+	/*test visualizacion*/
+	@Test
+	public void mensajeObtenerVisualizacion() {
+		String messageContents = "prueba de contenido de mensaje";
+		String messageContentsName = "nombre del contenido";
+		Mensaje mensaje = new Mensaje(new Texto(messageContentsName, messageContents));
+		assertEquals(messageContents, mensaje.obtenerVisualizacion());
+	}
+
+	/*test buscar*/
+	@Test
+	public void mensajeBuscarTest() {
+		String messageContents = "prueba de contenido de mensaje";
+		String messageContentsName = "nombre del contenido";
+		Mensaje mensaje = new Mensaje(new Texto(messageContentsName, messageContents));
+		assertEquals(mensaje, mensaje.buscar("prueba").toArray()[0]);
 	}
 }
