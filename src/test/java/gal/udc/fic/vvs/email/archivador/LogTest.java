76b17db4d1b4903a23f4a74faf3e9723dff4d294
Antón Valladares <anton.valladares@udc.es>
2021-01-04 15:14:52 +0100

    Include missing coverage in DecoradorArchivador.java

--- src/test/java/gal/udc/fic/vvs/email/archivador/LogTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivador/LogTest.java
@@ -4,8 +4,7 @@ import gal.udc.fic.vvs.email.archivo.Texto;
 import gal.udc.fic.vvs.email.correo.Mensaje;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class LogTest {
 
@@ -14,12 +13,22 @@ public class LogTest {
 	String nombreArchivador = "archivadorSimple";
 	int espacioArchivador = 10;
 	ArchivadorSimple archivadorSimple = new ArchivadorSimple(nombreArchivador, espacioArchivador);
+	ArchivadorSimple archivadorSimple2 = new ArchivadorSimple(nombreArchivador, espacioArchivador + 100);
 
 
 	@Test
 	public void logTest() {
 		Log log = new Log(archivadorSimple);
 		assertNotNull(log);
+	}
+
+	@Test
+	public void obtenerDelegadoTest() {
+		Delegado delegado = new Delegado(archivadorSimple2);
+		Log log = new Log(delegado);
+		Delegado delegado2 = new Delegado(archivadorSimple2);
+		log.establecerDelegado(delegado2);
+		assertEquals(delegado2, log.obtenerDelegado());
 	}
 
 	@Test
