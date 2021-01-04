9ac250ef088a111d7fe31f8990026d2e9fa4f704
Antón Valladares <anton.valladares@udc.es>
2021-01-04 17:15:42 +0100

    Added remaining tests for 100% coverage of DecoradorMensaje.java

--- src/main/java/gal/udc/fic/vvs/email/correo/DecoradorMensaje.java
+++ src/main/java/gal/udc/fic/vvs/email/correo/DecoradorMensaje.java
@@ -4,70 +4,70 @@ import java.util.Collection;
 
 public abstract class DecoradorMensaje extends MensajeAbstracto {
 
-    public DecoradorMensaje(MensajeAbstracto decorado) {
-        _decorado = decorado;
-    }
+	public DecoradorMensaje(MensajeAbstracto decorado) {
+		_decorado = decorado;
+	}
 
-    public void establecerLeido(boolean leido) {
-        _decorado.establecerLeido(leido);
-    }
+	public void establecerLeido(boolean leido) {
+		_decorado.establecerLeido(leido);
+	}
 
-    public int obtenerNoLeidos() {
-        return _decorado.obtenerNoLeidos();
-    }
+	public int obtenerNoLeidos() {
+		return _decorado.obtenerNoLeidos();
+	}
 
-    public int obtenerTamaño() {
-        return _decorado.obtenerTamaño();
-    }
+	public int obtenerTamaño() {
+		return _decorado.obtenerTamaño();
+	}
 
-    public Integer obtenerIcono() {
-        return _decorado.obtenerIcono();
-    }
+	public Integer obtenerIcono() {
+		return _decorado.obtenerIcono();
+	}
 
-    public String obtenerPreVisualizacion() {
-        return _decorado.obtenerPreVisualizacion();
-    }
+	public String obtenerPreVisualizacion() {
+		return _decorado.obtenerPreVisualizacion();
+	}
 
-    public String obtenerVisualizacion() {
-        return _decorado.obtenerVisualizacion();
-    }
+	public String obtenerVisualizacion() {
+		return _decorado.obtenerVisualizacion();
+	}
 
-    public String obtenerRuta() {
-        return _decorado.obtenerRuta();
-    }
+	public String obtenerRuta() {
+		return _decorado.obtenerRuta();
+	}
 
-    public Collection explorar() throws OperacionInvalida {
-        return _decorado.explorar();
-    }
+	public Collection explorar() throws OperacionInvalida {
+		return _decorado.explorar();
+	}
 
-    public Collection buscar(String busqueda) {
-        Collection resultado = _decorado.buscar(busqueda);
-        if (resultado.remove(_decorado)) {
-            resultado.add(this);
-        }
-        return resultado;
-    }
+	public Collection buscar(String busqueda) {
+		Collection resultado = _decorado.buscar(busqueda);
+		if (resultado.remove(_decorado)) {
+			resultado.add(this);
+		}
+		return resultado;
+	}
 
-    public void añadir(Correo correo) throws OperacionInvalida {
-        _decorado.añadir(correo);
-    }
+	public void añadir(Correo correo) throws OperacionInvalida {
+		_decorado.añadir(correo);
+	}
 
-    public void eliminar(Correo correo) throws OperacionInvalida {
-        _decorado.eliminar(correo);
-    }
+	public void eliminar(Correo correo) throws OperacionInvalida {
+		_decorado.eliminar(correo);
+	}
 
-    public Correo obtenerHijo(int n) throws OperacionInvalida {
-        return _decorado.obtenerHijo(n);
-    }
+	public Correo obtenerHijo(int n) throws OperacionInvalida {
+		return _decorado.obtenerHijo(n);
+	}
 
-    public Correo obtenerPadre() {
-        return _decorado.obtenerPadre();
-    }
+	public Correo obtenerPadre() {
+		return _decorado.obtenerPadre();
+	}
 
-    protected void establecerPadre(Correo padre) {
-        _decorado.establecerPadre(padre);
-    }
+	protected void establecerPadre(Correo padre) {
+		_decorado.establecerPadre(padre);
+	}
 
-    private MensajeAbstracto _decorado;
+	private MensajeAbstracto _decorado;
 
 }
