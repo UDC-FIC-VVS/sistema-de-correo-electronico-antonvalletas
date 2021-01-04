5bfe6dadb54e39b4e466286a8be74db98a8f34fc
Antón Valladares <anton.valladares@udc.es>
2021-01-04 15:15:38 +0100

    Include missing coverage Texto.java

--- src/test/java/gal/udc/fic/vvs/email/archivo/TextoTest.java
+++ src/test/java/gal/udc/fic/vvs/email/archivo/TextoTest.java
@@ -17,6 +17,12 @@ public class TextoTest {
 	}
 
 	@Test
+	public void obtenerNombreTest() {
+		Texto texto = new Texto(textName, textContents);
+		assertEquals(textName, texto.obtenerNombre());
+	}
+
+	@Test
 	public void obtenerMimeTypeTest() {
 		Texto texto = new Texto(textName, textContents);
 		assertEquals("text/plain", texto.obtenerMimeType());
